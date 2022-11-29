68598d215f5b416e136cbf54d9e4d7c193b342a5
Harash_1421 <abdulaziz.harash@hotmail.com>
2022-11-29 09:49:26 +0300

    Add Details Screen

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
       title: 'Meditation',
       theme: ThemeData(
         primarySwatch: Colors.blue,
